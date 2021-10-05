0dd5c271d2eb12c6badde8cb7566fe2133b3973f
cristianhernandez <cchernandezsilva@ucundinamarca.edu.co>
2021-10-05 09:54:44 -0500

    second commit

--- src/app/pages/vehiculo/vehiculo.component.ts
+++ src/app/pages/vehiculo/vehiculo.component.ts
@@ -1,52 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Vehiculo } from 'src/app/_model/Vehiculo';
-import { VehiculoService } from 'src/app/_service/vehiculo.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { Ciudad } from 'src/app/_model/Ciudad';
+import { Departamento } from 'src/app/_model/Departamento';
+import { DepartamentoService } from 'src/app/_service/departamento.service';
 
 @Component({
   selector: 'app-vehiculo',
   templateUrl: './vehiculo.component.html',
   styleUrls: ['./vehiculo.component.css']
 })
-export class VehiculoComponent implements OnInit {
+export class DepartamentoComponent implements OnInit {
 
-  constructor(private vehiculoService: VehiculoService,
-              private snackBar: MatSnackBar) { }
+  displayedColumns: string[] = ['codigo', 'nombre', 'ver'];
+  dataSource = new MatTableDataSource<Vehiculo>();
+
+  @ViewChild("vehiculoPaginator") paginator: MatPaginator;
+
+  constructor(private departamentoService: DepartamentoService,
+              public route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
-    let vehiculo: Vehiculo = new Vehiculo();
-    vehiculo.idVehiculo = 5;
-    vehiculo.placa = "akm-147";
-    vehiculo.modelo = "2019";
-    vehiculo.marca = "Ford";
-    vehiculo.tipoVehiuclo = "Carga";
-    vehiculo.capacidad = "50Kg"; 
-
-    /*this.vehiculoService.guardar(vehiculo).subscribe(data =>{
-        console.log("Se registro vehiculo");
-    });*/
-
-     this.vehiculoService.editar(vehiculo).subscribe(data =>{
-        console.log("Vehiculo editado correctamente");
-      }, err => {
-        if(err.error.status == 500) {
-          this.openSnackBar("Error inesperado, comuniquese con el administrador");
-        } else{
-          this.openSnackBar(err.error.message);
-        }
-        
-      });
-
-
-  }
-
-  private openSnackBar(mensaje: string) {
-    this.snackBar.open(mensaje, 'Información', {
-      duration: 2000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
+    this.departamentoService.listar().subscribe(data =>{
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
     });
+
   }
+
 
 }
