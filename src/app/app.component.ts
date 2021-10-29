87153ecd35aa19d98ccd02827e071dd78464e305
cristianhernandez <cchernandezsilva@ucundinamarca.edu.co>
2021-10-29 00:02:28 -0500

    se ajusta el toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-//import { LoaderService } from './loader/loader.service';
 import { BarraDeProgresoService } from './_service/barra-de-progreso.service';
 import { LoginService } from './_service/login.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
 
   public flagProgressBar: boolean = true;
 
@@ -21,11 +21,10 @@ export class AppComponent implements OnInit {
     private barraDeProgresoService: BarraDeProgresoService,
     public route: ActivatedRoute,
     public loginService: LoginService
-  ) { }
+  ){}
 
   ngOnInit(): void {
     this.barraDeProgresoService.progressBarReactiva.subscribe(data => {
-      // this.flacProgressBar = data;
       this.flagProgressBar = !this.flagProgressBar;
       this.isLoggedIn$ = this.loginService.isLoggedIn;
     });
